b77ab2b3e1a1d1e5875c139304db6a85f36056c0
RICHARD Sébastien <sebastien-richard3@etud.univ-tln.fr>
2024-03-27 10:55:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# JavaFX Sample Maven Project
+a simple sample of the use of JavaFX with Maven
+
+## Build
+Enter the following commands:
+
+`mvn clean`
+
+`mvn compile`
+
+`mvn package`
+
+## Run
+The following commands are running the official tutorials
+
+1. [Hello World](https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/hello_simpleapplication.html): 
+`java -cp "target/jmonkey-1.0.0-SNAPSHOT-dist/*" fr.utln.jmonkey.tutorials.HelloWorld`
+
+2. [Hello Node](https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/hello_node.html): 
+`java -cp "target/jmonkey-1.0.0-SNAPSHOT-dist/*" fr.utln.jmonkey.tutorials.HelloNode`
+
+2. [Hello Assets](https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/hello_asset.html): 
+`java -cp "target/jmonkey-1.0.0-SNAPSHOT-dist/*" fr.utln.jmonkey.tutorials.HelloAssets`
+
+## Tutorials
+[https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/beginner.html](https://wiki.jmonkeyengine.org/docs/3.4/tutorials/beginner/beginner.html)
---
+++ src/main/java/fr/utln/jmonkey/tutorials/beginner/HelloLoop.java
@@ -0,0 +1,60 @@
+package fr.utln.jmonkey.tutorials.beginner;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/** Sample 4 - how to trigger repeating actions from the main event loop.
+ * In this example, you use the loop to make the player character
+ * rotate continuously. */
+public class HelloLoop extends SimpleApplication {
+
+    public static void main(String[] args){
+        HelloLoop app = new HelloLoop();
+        app.start();
+    }
+
+    private Geometry player;
+
+    @Override
+    public void simpleInitApp() {
+        /** this blue box is our player character */
+        Box b = new Box(1, 1, 1);
+        player = new Geometry("blue cube", b);
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        player.setMaterial(mat);
+        rootNode.attachChild(player);
+        initKeys();
+    }
+
+
+
+    private void initKeys(){
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_H));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_L));
+
+        inputManager.addListener(analogListener, "Up", "Down");
+    }
+
+    private AnalogListener analogListener = new AnalogListener() {
+        @Override
+        public void onAnalog(String s, float value, float tpf) {
+            if (s.equals("Up")) {
+                player.move(new Vector3f(0, -value, 0));
+            }
+            if (s.equals("Down")) {
+                player.move(new Vector3f(0, 0, value));
+            }
+        }
+
+    };
+}
